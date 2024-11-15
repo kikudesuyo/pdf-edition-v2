f2042ab2bd418ffba98f5fa2bad6124b753993e6
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-11-15 22:33:08 +0900

    modify header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,15 @@
+import HomeIcon from "@/assets/icons/HomeIcon";
+
 const Header = () => {
   return (
-    <header className="sticky top-0 flex h-16 border-b-2 border-black  p-3 shadow-md">
+    <header className="sticky top-0 flex h-16 border-b-2 p-3 shadow-lg items-center">
       <div className="flex flex-row gap-16">
-        <p className="text-cyan-600">Top</p>
-        <p>結合</p>
-        <p>分割</p>
+        <div className="flex flex-row items-center gap-2 text-slate-600">
+          <HomeIcon size="size-8" />
+          <p className="font-bold text-2xl">Home</p>
+        </div>
+        <p className="font-semibold text-xl text-slate-600 p-2">PDF 結合</p>
+        <p className="font-semibold text-xl text-slate-600 p-2">PDF 分割</p>
       </div>
     </header>
   );
