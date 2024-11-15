0b675807b7eddae9bec3c42a1600ebafd0cb56b6
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-11-15 16:36:40 +0900

    アーキテクチャ構築

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,9 +2,9 @@ const Header = () => {
   return (
     <header className="top-0 flex h-16 border-b-2 border-black  p-3 shadow-md">
       <div className="flex flex-row gap-16">
-        <a href="">Top</a>
-        <a href="">結合</a>
-        <a href="">分割</a>
+        <p className="text-cyan-600">Top</p>
+        <p>結合</p>
+        <p>分割</p>
       </div>
     </header>
   );
