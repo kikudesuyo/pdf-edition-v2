7d3ec44d0c9aa82f9af59d2b08091dbb2514c21e
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-11-15 22:34:15 +0900

    modify footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -6,15 +6,18 @@ const Footer = () => {
     <footer className="flex flex-col justify-center items-center text-center sticky bottom-0 bg-slate-200 gap-4 pt-8">
       <div className="flex flex-row gap-4">
         <EmailIcon size="size-8" />
-        <p className="font-semibold">Contact:</p>
-        <a href="mailto:h.kiku1224@gmail.com" className="font-semibold">
+        <p className="font-semibold text-xl">Contact:</p>
+        <a href="mailto:h.kiku1224@gmail.com" className="font-semibold text-xl">
           h.kiku1224@gmail.com
         </a>
       </div>
       <div className="flex flex-row gap-4">
         <GitHubIcon size="size-8" />
-        <p className="font-semibold">Github:</p>
-        <a href="https://github.com/kikudesuyo" className="font-semibold">
+        <p className="font-semibold text-xl">Github:</p>
+        <a
+          href="https://github.com/kikudesuyo"
+          className="font-semibold text-xl"
+        >
           https://github.com/kikudesuyo
         </a>
       </div>
