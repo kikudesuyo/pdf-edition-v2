a9eb2854d3ea6348f06c7abb714d69196f6b4062
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-11-15 15:53:36 +0900

    ヘッダー定義

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="top-0 flex h-16 border-b-2 border-black  p-3 shadow-md">
+      <div className="flex flex-row gap-16">
+        <a href="">Top</a>
+        <a href="">結合</a>
+        <a href="">分割</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
