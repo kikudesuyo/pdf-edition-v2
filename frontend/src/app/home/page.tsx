6f778dedd8adff34406ab70ba7273105d2cc8644
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-11-24 23:55:30 +0900

    UI修正

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,34 +1,35 @@
 "use client";
 
 import { Text } from "@yamada-ui/react";
-import { mergePdf } from "@/service/api";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="mt-12 flex flex-col items-center justify-center text-black">
-      <div className="max-w-3xl text-center">
+      <div className="flex max-w-3xl flex-col gap-12 text-center">
         <h1 className="mb-4 text-4xl font-extrabold">
           PDFファイルの編集を簡単に
         </h1>
-        <p className="mb-6 text-lg">
-          あなたのPDFファイルを簡単に編集。
-          <br />
-          無料で利用でき、データの保存は一切行いませんので、安心してご利用いただけます。
-        </p>
         <div>
-          <Text>機能一覧</Text>
-          <ul>
-            <li>PDFファイルの結合</li>
-            <li>PDFファイルの分割</li>
-            <li>Coming soon...</li>
-          </ul>
+          <Text className="mb-6 text-2xl font-semibold">
+            あなたのPDFファイルを簡単に編集
+          </Text>
+          <Text className="mb-6 text-xl font-semibold">
+            データの保存は一切行わないので、安心してご利用いただけます。
+          </Text>
         </div>
-        <button
-          className="mt-8 rounded-lg bg-blue-500 px-6 py-3 text-white shadow-lg hover:bg-blue-600"
-          onClick={mergePdf}
-        >
-          PDFファイルを結合する
-        </button>
+        <div className="flex flex-row items-center justify-center gap-12">
+          <Link href="/split-pdf">
+            <p className="rounded-md bg-red-500 p-4 text-2xl font-bold text-white">
+              PDFファイル分割
+            </p>
+          </Link>
+          <Link href="/merge-pdf">
+            <p className="rounded-lg bg-red-500 p-4 text-2xl font-bold text-white">
+              PDFファイル結合
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
