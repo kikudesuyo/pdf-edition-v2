912f0763c28ae5de5ac506ab800d79e396850775
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-02-21 00:56:44 +0900

    refactor: ロジック修正

--- backend/internal/pdf/merge.go
+++ backend/internal/pdf/merge.go
@@ -9,22 +9,18 @@ import (
 
 func MergePDF(blobs [][]byte) ([]byte, error) {
 	var mergedPDF bytes.Buffer
-	var pdfDocs []*model.PdfReader
+	pdfWriter := model.NewPdfWriter()
 	for _, blob := range blobs {
 		pdfReader, err := model.NewPdfReader(bytes.NewReader(blob))
 		if err != nil {
 			return nil, fmt.Errorf("PDFの読み込みに失敗しました: %v", err)
 		}
-		pdfDocs = append(pdfDocs, pdfReader)
-	}
-	pdfWriter := model.NewPdfWriter()
-	for _, pdfDoc := range pdfDocs {
-		numPages, err := pdfDoc.GetNumPages()
+		numPages, err := pdfReader.GetNumPages()
 		if err != nil {
 			return nil, fmt.Errorf("ページ数の取得に失敗しました: %v", err)
 		}
 		for i := 1; i <= numPages; i++ {
-			page, err := pdfDoc.GetPage(i)
+			page, err := pdfReader.GetPage(i)
 			if err != nil {
 				return nil, fmt.Errorf("ページの取得に失敗しました: %v", err)
 			}
