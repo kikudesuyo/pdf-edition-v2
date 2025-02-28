91abf947516311ed717d5a3da99dfaf4ae1df1c6
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-03-01 07:51:39 +0900

    fix: split修正

--- backend/api/split_pdf/index.go
+++ backend/api/split_pdf/index.go
@@ -1,28 +1,15 @@
 package api
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
-)
 
-type Hello struct {
-	ID      string `json:"id"`
-	Message string `json:"message"`
-}
+	"github.com/kikudesuyo/pdf-edition-v2/backend/handler"
+)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case http.MethodGet:
-		h := Hello{
-			ID:      "1",
-			Message: "split!!",
-		}
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(h); err != nil {
-			log.Println(err)
-		}
+	case http.MethodPost:
+		handler.SplitPDFHandler(w, r)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
