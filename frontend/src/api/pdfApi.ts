deaf385f1b9335ad73c9dd6585b82ef702eb97c1
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-06-04 17:24:27 +0900

    refactor

--- frontend/src/api/pdfApi.ts
+++ frontend/src/api/pdfApi.ts
@@ -5,7 +5,8 @@ export const mergePdf = async (files: File[]) =>
 
 export const splitPdf = async (files: File[]) => {
   if (files.length !== 1) {
-    throw new Error("PDFの分割は1ファイルのみ対応しています。");
+    alert("PDFの分割は1ファイルのみ対応しています。");
+    return;
   }
   downloadFile("/api/split-pdf", files, "split_pdfs.zip");
 };
@@ -18,16 +19,13 @@ const downloadFile = async (
   try {
     const formData = new FormData();
     files.forEach((file) => formData.append("files", file));
-
     const blob = await requestPost<Blob>(endpoint, formData, {
       responseType: "blob",
     });
-
     const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
     link.download = filename;
     link.click();
-
     // メモリリーク防止にURLを解放
     URL.revokeObjectURL(link.href);
   } catch (error) {
